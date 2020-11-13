2ac5e5bbd206bb7b5dea074b20591445defb199f
Chris Norman <chris.norman2@googlemail.com>
2020-11-13 19:11:34 +0000

    Added an angle_between function, thanks to Walter Plinge.

--- movement_2d/__init__.py
+++ movement_2d/__init__.py
@@ -1,10 +1,10 @@
 """This package contains various functions pertaining to 2d movement."""
 
-from math import cos, pi, sin
+from math import atan2, cos, pi, sin
 from typing import List, Tuple
 
 __all__: List[str] = [
-    'normalise_angle', 'angle2rad', 'coordinates_in_direction'
+    'normalise_angle', 'angle2rad', 'coordinates_in_direction', 'angle_between'
 ]
 
 
@@ -33,3 +33,32 @@ def coordinates_in_direction(
     x: float = start_x + (distance * sin(rad))
     y: float = start_y + (distance * cos(rad))
     return x, y
+
+
+def angle_between(
+    x1: float, y1: float,
+    x2: float, y2: float
+) -> float:
+    """Returns the angle between two points.
+
+    For example::
+        angle_between(0, 0, 1, 1) == 45
+
+    Code provided by Walter Plinge.
+    """
+    angle: float
+    # Check if the points are on top of each other and output something reasonable.
+    if x1 == x2 and y1 == y2:
+        return 0
+    # If y1 and y2 are the same, we'll end up dividing by 0, and that's bad.
+    if y1 == y2:
+        if x2 > x1:
+            return 90
+        else:
+            return 270
+    angle = atan2(x2 - x1, y2 - y1)
+    # Convert result from radians to degrees. If you want minutes and seconds as well it's tough.
+    angle = angle * 180 / pi
+    # Ensure the angle is between 0 and 360.
+    angle = normalise_angle(angle)
+    return angle
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,4 @@
-from movement_2d import coordinates_in_direction, normalise_angle
+from movement_2d import coordinates_in_direction, normalise_angle, angle_between
 
 
 def test_normalise_angle():
@@ -14,3 +14,15 @@ def test_coordinates_in_direction():
     assert coordinates_in_direction(3.0, 3.0, 90) == (4.0, 3.0)
     assert coordinates_in_direction(3.0, 3.0, 180) == (3.0, 2.0)
     assert coordinates_in_direction(3.0, 3.0, 270) == (2.0, 3.0)
+
+
+def test_angle_between():
+    assert angle_between(0, 0, 0, 0) == 0
+    assert angle_between(4, 4, 4, 4) == 0
+    assert angle_between(0, 0, 1, 1) == 45
+    assert angle_between(1, 1, 2, 1) == 90
+    assert angle_between(5, 6, 6, 5) == 135
+    assert angle_between(3, 4, 3, 2) == 180
+    assert angle_between(-2, -2, -3, -3) == 225
+    assert angle_between(0, 8, -2, 8) == 270
+    assert angle_between(10, 10, 10, 13) == 0
